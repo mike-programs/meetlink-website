d322dfc115fab44ca628247830904769a3e41f04
Michael Mathew <michaelmathew34@gmail.com>
2023-05-27 19:50:48 +0100

    updated reveal package to accomodate peer dependency conflict

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import LearnMore from './LearnMore'
-// @ts-ignore
-import Zoom from 'react-reveal/Zoom'
+import { Zoom } from "react-awesome-reveal";
 
 interface Props {
     title: string,
@@ -14,15 +13,15 @@ interface Props {
 export default function Features(props: Props) {
     return (
         <>
-            <div className={'mx-4 mt-28 md:justify-between md:mx-28 md:my-32 ' + props.style}>
+            <div style={{ backfaceVisibility: 'hidden' }} className={'mx-4 mt-28 md:justify-between md:mx-28 md:my-32 ' + props.style}>
                 <div className='md:w-[40%] self-center'>
                     <p className='text-primaryColor text-3xl font-bold mb-8'>{props.title}</p>
                     <span className='text-text leading-7'>{props.content}</span>
                     <LearnMore />
                 </div>
-                <div className='mt-28 md:w-[50%] md:my-auto'>
 
-                    <Zoom>
+                <div className='mt-28 md:w-[50%] md:my-auto'>
+                    <Zoom triggerOnce fraction={0} duration={1500} cascade>
                         <img src={props.img} alt={props.alt} />
                     </Zoom>
                 </div>
