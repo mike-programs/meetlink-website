40583b86f8cf001cba2effb7811d00ff905f6fba
Michael Mathew <michaelmathew@Michaels-MacBook-Pro.local>
2024-10-26 21:43:46 +0100

    responsive on bigger screens

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
+    <div className="max-w-[1800px] mx-auto">
       <Nav />
       <HeroPage />
       <Section />
